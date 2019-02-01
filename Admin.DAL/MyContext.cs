4880510fac5dcb705283ab6a8d3d67df0ee43678
ismail Çadır <ismailcadir@gmail.com>
2019-02-01 12:24:41 +0300

    Add project files.

---
+++ Admin.DAL/MyContext.cs
@@ -0,0 +1,49 @@
+﻿using Admin.Models.Entities;
+using System;
+using System.Data.Entity;
+
+namespace Admin.DAL
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+            : base("name=MyCon")
+        {
+            this.InstanceDate = DateTime.Now;
+        }
+
+        public DateTime InstanceDate { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>()
+                .Property(x => x.TaxRate)
+                .HasPrecision(4, 2);
+
+            modelBuilder.Entity<Product>()
+                .Property(x => x.BuyPrice)
+                .HasPrecision(7, 2);
+
+            modelBuilder.Entity<Product>()
+                .Property(x => x.SalesPrice)
+                .HasPrecision(7, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(x => x.Quantity)
+                .HasPrecision(8, 4);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(x => x.Price)
+                .HasPrecision(9, 3);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(x => x.Discount)
+                .HasPrecision(3, 2);
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Invoice> Invoices { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Admin.Web.UI/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Admin.BLL.Services;
+
+namespace Admin.Web.UI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            new BarcodeService().Get("8690698032203");
+        }
+    }
+}
