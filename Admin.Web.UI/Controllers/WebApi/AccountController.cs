6da0c67e101a3a49b77317e4685e57ac1b651ec8
ismail ÇADIR <ismailcadir@gmail.com>
2019-03-03 23:48:44 +0300

    web api entegre ok

---
+++ Admin.Web.UI/Controllers/WebApi/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Admin.BLL.Identity;
+using Admin.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Web;
+using System.Web.Http;
+
+namespace Admin.Web.UI.Controllers.WebApi
+{
+    public class AccountController : ApiController
+    {
+        [Authorize]
+        public IHttpActionResult GetLoginInfo()
+        {
+            var userManager = MembershipTools.NewUserManager();
+            var user = userManager.FindById(HttpContext.Current.User.Identity.GetUserId());
+            return Ok(new UserProfileViewModel()
+            {
+                Name = user.Name,
+                UserName = user.UserName,
+                Email = user.Email,
+                AvatarPath = user.AvatarPath,
+                Surname = user.Surname,
+                Id = user.Id
+            });
+        }
+    }
+}
