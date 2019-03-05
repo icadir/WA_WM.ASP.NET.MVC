fcfc815adce324a0ffd869fe460b21fe1ce9a161
ismail Çadır <ismailcadir@gmail.com>
2019-03-05 09:42:52 +0300

    Angular , devex Denme test ok.

---
+++ DevxOdata/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevxOdata.Models
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext()
+          : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
