eee8ce72618409b8db8c69ec50bbd057759ccec6
ismail Çadır <ismailcadir@gmail.com>
2019-03-05 12:27:30 +0300

    app js ok düzeltmeler yapılcak.

---
+++ DevxOdata/Controllers/WebApii/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using DevxOdata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DevxOdata.Controllers.WebApii
+{
+    public class CustomerController : ApiController
+    {
+  
+        public IHttpActionResult GetAll()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            return Ok(new
+            {
+                success = true,
+                data = db.Customers.OrderBy(x=>x.Name).ToList()
+            });
+        }
+    }
+}
--- DevxOdata/Scripts/app.js
+++ DevxOdata/Scripts/app.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="angular.js" />
 
 var app = angular.module("myApp", ["dx"]);
-
+var host = 'http://localhost:51106/';
 app.controller("testCtrl", function ($scope) {
 
     $scope.dataGridOptions = {
@@ -10,6 +10,70 @@ app.controller("testCtrl", function ($scope) {
         showBorders: true
     };
 
+});
+
+app.controller("customerCtrl", function ($scope, $http) {
+    $scope.data = null;
+    function init() {
+        $http({
+            url: host + 'api/customer/getall',
+            method: 'GET'
+        }).then(function (ev) {
+            if (ev.data.success) {
+                $scope.data = ev.data.data;
+                loadGrid();
+            }
+        });
+    }
+
+    function loadGrid() {
+        $scope.dataGridOptions = {
+            dataSource: $scope.data,
+            selection: {
+                mode: "multiple"
+            },
+            onSelectionChanged: function (selected) {
+                $scope.selected = selected.selectedRowsData;
+            },
+            "export": {
+                enabled: true,
+                fileName: "Customers_" + parseInt(Math.random() * 100000),
+                allowExportSelectedData: true
+            },
+            columnChooser: {
+                enabled: true,
+                allowSearch: true
+            },
+            columns: [
+                {
+                    dataField: "Id",
+                    caption: "Müşteri No",
+                    visible: false
+                }, "Name", "Surname", "Phone", "Address",
+                {
+                    dataField: "Balance",
+                    caption: "Balance",
+                    dataType: "number",
+                    format: "#,##0.## ₺"
+                }],
+            showBorders: true,
+            paging: {
+                pageSize: 10
+            },
+            pager: {
+                showPageSizeSelector: true,
+                allowedPageSizes: [5, 10, 20],
+                showInfo: true
+            },
+            searchPanel: {
+                visible: true,
+                width: 240,
+                placeholder: "Ara..."
+            }
+        };
+    }
+
+    init();
 });
 
 var customers = [{
