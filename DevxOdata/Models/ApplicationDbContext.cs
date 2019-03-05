b9c6adef4b3c50f86be766af16264128070d8f59
ismail Çadır <ismailcadir@gmail.com>
2019-03-05 09:49:46 +0300

    fake data ok.

--- DevxOdata/Models/ApplicationDbContext.cs
+++ DevxOdata/Models/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace DevxOdata.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
           : base("DefaultConnection", throwIfV1Schema: false)
