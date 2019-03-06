f865e27991c9c5394498a23370032953c89d582e
ismail Çadır <ismailcadir@gmail.com>
2019-03-06 13:50:13 +0300

    rabbitmq Test ok

--- Rabbit.Web.Mvc/Models/Publisher.cs
+++ Rabbit.Web.Mvc/Models/Publisher.cs
@@ -11,23 +11,23 @@ namespace Rabbit.Web.Mvc.Models
     public class Publisher
     {
         private readonly RabbitMqService _rabbitMqService;
-        private const string DefaultQeue = "wissen1";
-
-        public Publisher(string queueName, string message)
+        private const string DefaultQueue = "wissen1";
+        public Publisher(string message, string queueName = null)
         {
             if (string.IsNullOrEmpty(queueName))
-                queueName = DefaultQeue;
+                queueName = DefaultQueue;
             _rabbitMqService = new RabbitMqService();
 
-            using ( var connection = _rabbitMqService.GetRabitMqConnection())
+            using (var connection = _rabbitMqService.GetRabbitMqConnection())
             {
-                using ( var channel = connection.CreateModel())
+                using (var channel = connection.CreateModel())
                 {
                     channel.QueueDeclare(queueName, false, false, false, null);
-                    channel.BasicPublish(string.Empty,queueName,null,Encoding.UTF8.GetBytes(message));
+                    channel.BasicPublish(string.Empty, queueName, null, Encoding.UTF8.GetBytes(message));
+
+                    //Console.WriteLine($"{queueName} queue'su üzerine, \"{message}\" mesajı yazıldı.");
                 }
             }
-
         }
     }
 }
--- Rabit.BLL/RabbitMq/RabbitMqService.cs
+++ Rabit.BLL/RabbitMq/RabbitMqService.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
+using System;
 
 namespace Rabit.BLL.RabbitMq
 {
     public class RabbitMqService
     {
         private readonly string _hostName = "localhost",
-            _userName = "guest",
-            _password = "guest";
+            _userName = "ismail",
+            _password = "123123";
 
-        public IConnection GetRabitMqConnection()
+     
+
+        public IConnection GetRabbitMqConnection()
         {
             ConnectionFactory connectionFactory = new ConnectionFactory
             {
+                // RabbitMQ'nun bağlantı kuracağı host'u tanımlıyoruz. Herhangi bir güvenlik önlemi koymak istersek, Management ekranından password adımlarını tanımlayıp factory içerisindeki "UserName" ve "Password" property'lerini set etmemiz yeterlidir.
                 HostName = _hostName,
                 VirtualHost = "/",
                 UserName = _userName,
                 Password = _password,
-                Uri=new Uri($"amqp://{_userName}:{_password}@{_hostName}")
+                Uri = new Uri($"amqp://{_userName}:{_password}@{_hostName}")
             };
             return connectionFactory.CreateConnection();
         }
- 
+    }
 
 
-   }
+
 }
+
