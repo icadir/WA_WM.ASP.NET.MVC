c542edefcdf0b26036257f72176180f93fce7bd2
ismail Çadır <ismailcadir@gmail.com>
2019-03-06 11:30:28 +0300

    Eksiklerle eklendi düzeltilecek

---
+++ Rabit.Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabit.Models
+{
+    public
+       class Customer
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Rabit.Models/MailLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabit.Models
+{
+    [Table("MailLogs")]
+    public class MailLog
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+
+        public string Message { get; set; }
+        public string Subject { get; set; }
+        public Guid CustomerId { get; set; }
+        
+
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+    }
+}
